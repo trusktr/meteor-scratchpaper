bb69e12f5fa3172ae490f7ff6e2f05f90340a84e
Joseph Orbegoso Pea <joe@trusktr.io>
2015-05-25 20:43:47 -0700

    Fixed wrong api.use

--- package.js
+++ package.js
@@ -28,6 +28,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
     api.use('tinytest')
-    api.use('trusktr:lib')
+    api.use('trusktr:scratchpaper')
     api.addFiles('lib-tests.js')
 })
