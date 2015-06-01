f6845e60676ba4d6046c7add57a15507053540a9
Joseph Orbegoso Pea <joe@trusktr.io>
2015-06-01 11:03:00 -0700

    added package.json

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'trusktr:scratchpaper',
-    version: '0.0.1',
+    version: '0.0.2',
     // Brief, one-line summary of the package.
     summary: 'A package for meteor that I can test random crap in.',
     // URL to the Git repository containing the source code for this package.
