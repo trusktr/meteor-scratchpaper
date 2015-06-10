e6b6755122df3b94e60252d0b580777d46ed1de2
Joseph Orbegoso Pea <joe@trusktr.io>
2015-06-09 17:28:08 -0700

    v0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'trusktr:scratchpaper',
-    version: '0.0.2',
+    version: '0.0.3',
     // Brief, one-line summary of the package.
     summary: 'A package for meteor that I can test random crap in.',
     // URL to the Git repository containing the source code for this package.
