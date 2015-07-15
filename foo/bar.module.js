dfbce34077fb168992fb7bb6455f63569d6ee611
Joseph Orbegoso Pea <joe@trusktr.io>
2015-07-15 14:08:22 -0700

    Update

--- foo/bar.module.js
+++ foo/bar.module.js
@@ -1,14 +1,5 @@
 // Write your package code here!
 var uppercase = require('upper-case')
-var local = require('./local')
 
 console.log(' --- ', uppercase('hello world (other.module.js)!'))
-console.log(' --- local import: ', local)
-
-// a Meteor export that other packages can use if they depend on this package.
-Lib = {
-    hello: function() {
-        return 'Hello.'
-    }
-}
 
